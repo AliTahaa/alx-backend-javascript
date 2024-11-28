8e829950b5d249f3a312294b152b7426099ce4dd
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-11-29 01:05:28 +0200

    task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('floating point', () => {
+  it('floating point whole numbers', () => {
     assert.strictEqual(calculateNumber(1.0, 2.0), 3);
   });
 
@@ -10,27 +10,27 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1.0, 2.4), 3);
   });
 
-  it('rounding down a and b\'s floating point', () => {
+  it('rounding down a and bs floating point', () => {
     assert.strictEqual(calculateNumber(1.4, 2.4), 3);
   });
 
-  it('rounding down a\'s floating point', () => {
+  it('rounding down as floating point', () => {
     assert.strictEqual(calculateNumber(1.4, 2.0), 3);
   });
 
-  it('rounding up b\'s floating point', () => {
-    assert.strictEqual(calculateNumber(4.0, 4.5), 4);
+  it('rounding up bs floating point', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.5), 4);
   });
 
-  it('rounding up a and b\'s floating point', () => {
-    assert.strictEqual(calculateNumber(4.6, 4.5), 6);
+  it('rounding up a and bs floating point', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.5), 6);
   });
 
-  it('rounding up a\'s floating point', () => {
-    assert.strictEqual(calculateNumber(4.6, 4.0), 5);
+  it('rounding up as floating point', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.0), 5);
   });
 
   it('rounding down a and b floating point with trailing 9s', () => {
-    assert.strictEqual(calculateNumber(1.499999, 2.499999), 5);
+    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
   });
 });
