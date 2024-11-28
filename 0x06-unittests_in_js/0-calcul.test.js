f026409ed4d84531ec4e7c1ddacef70ed3f718ae
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-11-29 01:02:57 +0200

    task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,35 +2,35 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('floating point whole numbers', () => {
+  it('floating point', () => {
     assert.strictEqual(calculateNumber(1.0, 2.0), 3);
   });
 
-  it('rounding down b\'s floating point fractional number', () => {
+  it('rounding down bs floating point', () => {
     assert.strictEqual(calculateNumber(1.0, 2.4), 3);
   });
 
-  it('rounding down a and b\'s floating point fractional number', () => {
+  it('rounding down a and b\'s floating point', () => {
     assert.strictEqual(calculateNumber(1.4, 2.4), 3);
   });
 
-  it('rounding down a\'s floating point fractional number', () => {
+  it('rounding down a\'s floating point', () => {
     assert.strictEqual(calculateNumber(1.4, 2.0), 3);
   });
 
-  it('rounding up b\'s floating point fractional numbers', () => {
-    assert.strictEqual(calculateNumber(1.0, 2.5), 4);
+  it('rounding up b\'s floating point', () => {
+    assert.strictEqual(calculateNumber(4.0, 4.5), 4);
   });
 
-  it('rounding up a and b\'s floating point fractional numbers', () => {
-    assert.strictEqual(calculateNumber(2.6, 2.5), 6);
+  it('rounding up a and b\'s floating point', () => {
+    assert.strictEqual(calculateNumber(4.6, 4.5), 6);
   });
 
-  it('rounding up a\'s floating point fractional numbers', () => {
-    assert.strictEqual(calculateNumber(2.6, 2.0), 5);
+  it('rounding up a\'s floating point', () => {
+    assert.strictEqual(calculateNumber(4.6, 4.0), 5);
   });
 
-  it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
-    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
+  it('rounding down a and b floating point with trailing 9s', () => {
+    assert.strictEqual(calculateNumber(1.499999, 2.499999), 5);
   });
 });
