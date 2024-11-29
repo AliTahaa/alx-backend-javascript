8f623f6f4420b2523f8d9e31d1bf2f409351719f
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-11-30 01:45:10 +0200

    task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -5,41 +5,41 @@ const hostname = '127.0.0.1';
 const port = 1245;
 
 function countStudents(fileName) {
-  const students = {};
-  const fields = {};
-  let length = 0;
+  const stds = {};
+  const fs = {};
+  let leng = 0;
   return new Promise((resolve, reject) => {
     readFile(fileName, (err, data) => {
       if (err) {
         reject(err);
       } else {
-        let output = '';
+        let out = '';
         const lines = data.toString().split('\n');
         for (let i = 0; i < lines.length; i += 1) {
           if (lines[i]) {
-            length += 1;
+            leng += 1;
             const field = lines[i].toString().split(',');
-            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-              students[field[3]].push(field[0]);
+            if (Object.prototype.hasOwnProperty.call(stds, field[3])) {
+              stds[field[3]].push(field[0]);
             } else {
-              students[field[3]] = [field[0]];
+              stds[field[3]] = [field[0]];
             }
-            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-              fields[field[3]] += 1;
+            if (Object.prototype.hasOwnProperty.call(fs, field[3])) {
+              fs[field[3]] += 1;
             } else {
-              fields[field[3]] = 1;
+              fs[field[3]] = 1;
             }
           }
         }
-        const l = length - 1;
-        output += `Number of students: ${l}\n`;
-        for (const [key, value] of Object.entries(fields)) {
+        const n = leng - 1;
+        out += `Number of students: ${n}\n`;
+        for (const [key, value] of Object.entries(fs)) {
           if (key !== 'field') {
-            output += `Number of students in ${key}: ${value}. `;
-            output += `List: ${students[key].join(', ')}\n`;
+            out += `Number of students in ${key}: ${value}. `;
+            out += `List: ${stds[key].join(', ')}\n`;
           }
         }
-        resolve(output);
+        resolve(out);
       }
     });
   });
