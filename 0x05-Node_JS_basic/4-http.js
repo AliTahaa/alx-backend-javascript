e37c1fb2d68b0c5685c4fbde381fdb26eb3b49f5
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-11-30 02:35:53 +0200

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 1245;
+const hostn = '127.0.0.1';
+const p = 1245;
 
 const app = http.createServer((request, response) => {
   response.statusCode = 200;
@@ -9,7 +9,7 @@ const app = http.createServer((request, response) => {
   response.end('Hello Holberton School!');
 });
 
-app.listen(port, hostname, () => {
+app.listen(p, hostn, () => {
 });
 
 module.exports = app;
